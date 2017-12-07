25112dff5e8c14a8878b87417754759c31d258cb
Speed Chan <speedchanjun@gmail.com>
2017-12-07 16:58:39 +0800

    updated css, inserted delays for game start and player switch

--- README.md
+++ README.md
@@ -1,96 +1,52 @@
-# Project Name (Start editing here)
-<!---
-Read Me Contents
--->
+# BE QUICK! ϟ
 
-# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) Project #1: The Game
+[Click here for a link to the game](https://knownopear.github.io/project-1/)
 
 ### Overview
 
-Let's start out with something fun - **a game!**
-
-Everyone will get a chance to **be creative**, and work through some really **tough programming challenges** – since you've already gotten your feet wet with Tic Tac Toe, it's up to you to come up with a fun and interesting game to build.
-
-**You will be working individually for this project**, but we'll be guiding you along the process and helping as you go. Show us what you've got!
-
+BE QUICK! ϟ is a fun and fast game developed with the intent of pitting its players against each other with questions of typing dexterity, mathematical skill and logic questions. There is no room for error, and once a player makes a mistake their turn is forfeited and the baton is passed on to the next player.
 
 ---
 
-### Technical Requirements
-
-Your app must:
-
-* **Render a game in the browser**
-* **Any number of players** will be okay, switch turns will be great 
-* **Design logic for winning** & **visually display which player won**
-* **Include separate HTML / CSS / JavaScript files**
-* Stick with **KISS (Keep It Simple Stupid)** and **DRY (Don't Repeat Yourself)** principles
-* Use **Javascript** for **DOM manipulation**, jQuery is not compulsory
-* **Deploy your game online**, where the rest of the world can access it
-* Use **semantic markup** for HTML and CSS (adhere to best practices)
-* **No canvas** project will be accepted, only HTML5 + CSS3 + JS please
+### Number of Players
+BE QUICK! ϟ currently supports 2 players
 
 ---
 
-### Necessary Deliverables
-
-* A **working game, built by you**, hosted somewhere on the internet
-* A **link to your hosted working game** in the URL section of your GitHub repo
-* A **git repository hosted on GitHub**, with a link to your hosted game, and frequent commits dating back to the very beginning of the project
-* **A ``readme.md`` file** with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+### Technologies Used
+* **HTML5**
+* **CSS3**
+* **JQuery**
 
 ---
 
-### Suggested Ways to Get Started
+### Development Approach
+At the beginning of development, I played around with the idea of a multiplayer follow-the-leader-type of game. However, I quickly realised that having multiple players simultaneously following a leader would require there to be multiple inputs of the same type. For example, if two players had to follow the same string of text, they would require two seperate keyboards for a simultaneous input. 
 
-* **Break the project down into different components** (data, presentation, views, style, DOM manipulation) and brainstorm each component individually. Use whiteboards!
-* **Use your Development Tools** (console.log, inspector, alert statements, etc) to debug and solve problems
-* Work through the lessons in class & ask questions when you need to! Think about adding relevant code to your game each night, instead of, you know... _procrastinating_.
-* **Commit early, commit often.** Don’t be afraid to break something because you can always go back in time to a previous version.
-* **Consult documentation resources** (MDN, jQuery, etc.) at home to better understand what you’ll be getting into.
-* **Don’t be afraid to write code that you know you will have to remove later.** Create temporary elements (buttons, links, etc) that trigger events if real data is not available. For example, if you’re trying to figure out how to change some text when the game is over but you haven’t solved the win/lose game logic, you can create a button to simulate that until then.
-
----
-
-### Potential Project Ideas
-
-##### Blackjack
-Make a one player game where people down on their luck can lose all their money by guessing which card the computer will deal next!
-
-##### Self-scoring Trivia
-Test your wits & knowledge with whatever-the-heck you know about (so you can actually win). Guess answers, have the computer tell you how right you are!
+With that in mind, I decided to have it as a turn-based game instead, with turns starting with player 1. I started writing the pseudo-code of what I envisoned the game to be - 
+* Player clicks a 'Start Game' button
+* There is a short delay
+* The game initialises
+* Copycat(the thing you have to follow) will generate and show a problem (math, logic, string, etc)
+* A timer will appear and count downwards
+* The player will have to answer within the time limit
+* If the timer reaches 0 or the player's answer is wrong, his turn ends
+* If the player's answer is correct, the game will continue indefinitely until the timer runs out or the answer is wrong
+* Timer resets with every correct answer
+* When the current player loses, record their score on a sidebar to show the next player what score they have to beat and change the players
+* There is a short delay
+* Initialise game and repeat steps
 
 ---
 
-### Useful Resources
-
-* **[MDN Javascript Docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript)** _(a great reference for all things Vanilla Javascript)_
-* **[jQuery Docs](http://api.jquery.com)** _(if you're using jQuery)_
-* **[GitHub Pages](https://pages.github.com)** _(for hosting your game)_
-* **[How to write readme - Markdown CheatSheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)** _(for editing this readme)_ 
-* **[How to write a good readme for github repo!](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)** _(to make it better)_
+### Plans Ahead
+I plan to allow for more than 2 players, and to increase the type of questions that can be endlessly generated. I also plan to shorten the code.
 
 ---
 
-### Project Feedback + Evaluation
+### Things I have learnt
+* console.log() & alert() are my best friends
+* google is the most useful tool a developer can have
+* if you are burnt out from hours of looking at code, take a break! It is okay to have some time off
 
-* __Project Workflow__: Did you complete the user stories, wireframes, task tracking, and/or ERDs, as specified above? Did you use source control as expected for the phase of the program you’re in (detailed above)?
-
-* __Technical Requirements__: Did you deliver a project that met all the technical requirements? Given what the class has covered so far, did you build something that was reasonably complex?
-
-* __Creativity__: Did you add a personal spin or creative element into your project submission? Did you deliver something of value to the end user (not just a login button and an index page)?
-
-* __Code Quality__: Did you follow code style guidance and best practices covered in class, such as spacing, modularity, and semantic naming? Did you comment your code as your instructors have in class?
-
-* __Deployment__: Did you deploy your application to a public url using GitHub Pages?
-
-* __Total__: Your instructors will give you a total score on your project between:
-
-    Score | Expectations
-    ----- | ------------
-    **0** | _Incomplete._
-    **1** | _Does not meet expectations._
-    **2** | _Meets expectations, good job!_
-    **3** | _Exceeds expectations, you wonderful creature, you!_
-
- This will serve as a helpful overall gauge of whether you met the project goals, but __the more important scores are the individual ones__ above, which can help you identify where to focus your efforts for the next project!
+---
--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
 $(document).ready(function(){
-/* There are 3 player slots available & one main "copycat" that they have to follow. After the main copycat does an action, the players have to do the same action within a certain time or lose. Actions can be a few things (typing in a string, solving a math equation, pressing certain buttons etc)
-
-Win variable: last one standing 
-Players: 2 (for now)
-one main copy for players to follow
-input box for all player to input their answers into
-
-----P S E U D O C O D E----
-
-1.) Player clicks start game button
-2.) There is a 3.. 2.. 1.. countdown
-3.) Game initialises
-4.) copycat will show a math problem, random text or something. A timer will appear 
-5.) players will answer within a time limit. if the answer is incorrect, 0 point and the player loses. 
-6.) game goes on indefinitely until player loses
-7.) when current player loses, record the score on the side bar
-8.) set current player to next player
-9.) there is a 3.. 2.. 1.. countdown
-10.) Repeat from step 4
-*/
-
-
 //~~~ G A M E  L O G I C ~~~//
 
 // ---------------------Start variables & functions--------------------- //
@@ -33,10 +11,10 @@ var currentPlayerScore = 0;
 var timeLeft = difficultyLevel;
 var gameRunning = true;
 var timer;
+var count = 0;
 
-// var copycatAnswer, playerInput, difficultyLevel, currentPlayerScore;
 
-
+//Saves player 1's score and dislays it to player 2 for comparison
 var saveScore = function(){
 	if(currentPlayer == 1){
 		player1Score += currentPlayerScore
@@ -47,14 +25,14 @@ var saveScore = function(){
 	}
 }
 
-//Every time difficulty level increases,
-// Length of text string increases, random numbers generated have a lower minimum and higher maximum
-// Maxmimum numbers in open-ended questions increase. 
+
+//Increases difficulty
 var increaseDifficulty = function(){
 	difficultyLevel ++;
 }
 
 
+//Create the open-ended math question
 var generateOpenEnded = function(){
 	var oefirstNumber = Math.floor(Math.random() * Math.pow(difficultyLevel, 3) + 500);
 	var oesecondNumber = Math.floor(Math.random() * Math.pow(difficultyLevel, 3) + 1);
@@ -69,17 +47,17 @@ var generateOpenEnded = function(){
 		var oegenerateFunctionNested = Math.floor(Math.random() * 3);
 		if(oegenerateFunctionNested == 0){
 			oefourthNumber = oethirdNumber + 10;
-			$("#questionBoxText").text("is " + oefirstNumber + " plus " + oesecondNumber + " equal to " + oefourthNumber + "?");
+			$("#questionBoxText").text("Is " + oefirstNumber + " plus " + oesecondNumber + " equal to " + oefourthNumber + "?");
 			copycatAnswer = "no";
 		}
 		else if(oegenerateFunctionNested == 1){
 			oefourthNumber = oethirdNumber + 100;
-			$("#questionBoxText").text("is " + oefirstNumber + " plus " + oesecondNumber + " equal to " + oefourthNumber + "?");	
+			$("#questionBoxText").text("Is " + oefirstNumber + " plus " + oesecondNumber + " equal to " + oefourthNumber + "?");	
 			copycatAnswer = "no";
 		}
 		else if(oegenerateFunctionNested == 2){
 			oefourthNumber = oethirdNumber + 1000;	
-			$("#questionBoxText").text("is " + oefirstNumber + " plus " + oesecondNumber + " equal to " + oefourthNumber + "?");
+			$("#questionBoxText").text("Is " + oefirstNumber + " plus " + oesecondNumber + " equal to " + oefourthNumber + "?");
 			copycatAnswer = "no";
 		}
 	}
@@ -107,12 +85,10 @@ var generateOpenEnded = function(){
 		$("#questionBoxText").text("Is " + oefirstNumber + " minus " + oesecondNumber + " equal to " + oefourthNumber + "?");
 		copycatAnswer = "yes";
 	}
-//is oefourth = oe2 + oe1? 
-//only 1/3 times is correct.
 }
 
 
-//generates the math equation
+//Creates the 'math' question
 var generateMathEquation = function(){
 	//To generate the numbers used in the question
 	var firstNumber = Math.floor(Math.random() * Math.pow(difficultyLevel, 3) + 500);
@@ -134,24 +110,22 @@ var generateMathEquation = function(){
 	copycatAnswer = answerNumber;
 }
 
-//to create the main string
+
+//Create the 'string' question, there are no capital 'i's
 var generateString = function() {
   var answer = "";
   var letters = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-
-  for (var i = 0; i < (difficultyLevel + 2); i++)
-    answer += letters.charAt(Math.floor(Math.random() * letters.length));
-
+  for (var i = 0; i < (difficultyLevel + 1); i++)
+	answer += letters.charAt(Math.floor(Math.random() * letters.length));
 	$("#questionBoxText").text("Type '" + answer + "'");
 	copycatAnswer = answer;
 }
 
 
-//randomly generates one new problem for the player
+//Generates a problem for the player
 var generateNewProb = function(){
-	
 	if(gameRunning == true){
-		var questionType = Math.floor(Math.random() * 5); // This returns a number from 0 - 5 (incusive)
+		var questionType = Math.floor(Math.random() * 5); // This returns a number from 0 - 5 (inclusive)
 		$("#countDownTimer").text(timeLeft + "s remaining");
 	
 			if(questionType === 0 || questionType === 1){
@@ -171,32 +145,34 @@ var generateNewProb = function(){
 }
 
 
+//Shows hidden divs for players to type in
 var allowInput = function(){
 	$("#timerDiv").show();
 	$("#curPlayerNScore").show(200);
-		$("#playerInputForm").show(200);
+	$("#playerInputForm").show(200);
 }
 
 
+//Runs a function that has an interval of 1second (for the timer)
 var runTimer = function(){
-	timeLeft = difficultyLevel; 
+	timeLeft = difficultyLevel + 2; 
 	timer = setInterval(function(){
 		timeLeft--;
 		$("#countDownTimer").text(timeLeft + "s remaining");
 		if(timeLeft <= 0){
-			clearTimeout(timer);
 			timerTimesOut();
 		}
-
 	}, 1000);
 }
 
+
+//Stops the timer
 var stopTimer = function(){
 	clearInterval(timer);
 }
 
 
-//checks the final score of players. highest wins
+//Compares player 1 & 2's scores
 var checkScore = function(){
 	$("#timerDiv").hide();
 	if(player1Score > player2Score){
@@ -216,39 +192,54 @@ var checkScore = function(){
 	}
 }
 
-
+//Runs a new game
 var newGame = function(){
-	stopTimer();
-	currentPlayer = 1;
-	player1Score = 0;
-	player2Score = 0;
-	currentPlayerScore = 0;
-	difficultyLevel = 5;
-	timeLeft = difficultyLevel;
+	$("#countDownTimer").hide();
 	gameRunning = true;
-	runTimer();
-	$("#playerHalf").show();
-	$("#playerInputBox").val("");
-	$("#displayCurrentScore").text("CURRENT SCORE: " + currentPlayerScore);
-	$("#displayCurrentPlayer").text("PLAYER: 1");
-	// $("#countDownTimer").text(timeLeft);
-	generateNewProb();
-	allowInput();
-	//initiate countdown function
+	$("#questionBoxText").text("Get ready Player 1!");
+	setTimeout(function(){
+		stopTimer();
+		count++;
+		currentPlayer = 1;
+		player1Score = 0;
+		player2Score = 0;
+		currentPlayerScore = 0;
+		difficultyLevel = 5;
+		timeLeft = difficultyLevel;
+		runTimer();
+		$("#playerHalf").show(200);
+		$("#countDownTimer").show();
+		$("#playerInputBox").val("");
+		$("#answerBoxText").text("");
+		$("#displayCurrentScore").text("CURRENT SCORE: " + currentPlayerScore);
+		$("#displayCurrentPlayer").text("PLAYER: 1");
+		if(count == 1){
+			generateNewProb();
+		}
+		allowInput();
+	}, 2000);
+	count = 0;
 }
 
 
+//Resets settings for player 2's turn
 var resetGame = function(){
 	stopTimer();
+	$("#countDownTimer").hide();
 	difficultyLevel = 5;
-	runTimer();
 	currentPlayer = 2;
 	currentPlayerScore = 0;
-	$("#scoreToBeatDiv").show(200);
-	$("#displayCurrentScore").text("CURRENT SCORE: " + currentPlayerScore);
-	$("#displayCurrentPlayer").text("PLAYER: 2");
-	//run a timer that counts down to player 2's turn
-	generateNewProb();
+	$("#playerHalf").hide();
+	$("#questionBoxText").text("Get ready Player 2!");
+	setTimeout(function(){
+		$("#playerHalf").show(200);
+		$("#countDownTimer").show();
+		runTimer();
+		$("#scoreToBeatDiv").show(200);
+		$("#displayCurrentScore").text("CURRENT SCORE: " + currentPlayerScore);
+		$("#displayCurrentPlayer").text("PLAYER: 2");
+		generateNewProb();
+	}, 2000);
 }
 
 
@@ -263,15 +254,12 @@ var timerTimesOut = function(){
 		checkScore();
 	}
 }
-// newGame(); //launches new Game
 
 
 // ---------------------End of variables & functions, start of logic---------------------//
 
-
-//create Countdown
-
-$("#newGameButton").click(function(){
+$("#newGameButton").click(function(e){
+	e.preventDefault();
 	newGame();
 });
 
@@ -301,24 +289,7 @@ $("#playerInputForm").on("submit", function(e){
 		saveScore();
 		checkScore();
 	}
-
 });
 
 
-
 }); //end jQuery
-
-
-
-
-
-/*
-when timer reaches 0, run function that checks if current player is player 1or 2. if player 1, resetGame();
-if player2, saveScore(); checkScore();
-
-*/
-
-
-
-
-
